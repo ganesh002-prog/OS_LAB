ca2bea07e9699a19fb131def5502eba22f1c8787
achari ganesh <11249a002@kanchiuniv.ac.in>
2026-04-20 21:31:12 +0530

    Create Biggest_of_three_numbers.sh

---
+++ exp 3/Biggest_of_three_numbers.sh
@@ -0,0 +1,16 @@
+PROGRAM:
+
+#!/bin/bash
+echo "Enter three numbers:"
+read a
+read b
+read c
+if [ $a -gt $b ] && [ $a -gt $c ]
+then
+echo "Biggest number is $a"
+elif [ $b -gt $c ]
+then
+echo "Biggest number is $b"
+else
+echo "Biggest number is $c"
+fi
