50e7b22c433aa65a3ffd6c87b2ca5ceda88db954
achari ganesh <11249a002@kanchiuniv.ac.in>
2026-04-20 21:33:14 +0530

    Add files via upload

---
+++ exp 3/exp 3b/Biggest_Of_Two_Numbers.sh
@@ -0,0 +1,13 @@
+PROGRAM:
+
+#!/bin/bash
+echo "Enter first number:"
+read a
+echo "Enter second number:"
+read b
+if [ $a -gt $b ]
+then
+echo "Biggest number is $a"
+else
+echo "Biggest number is $b"
+fi
